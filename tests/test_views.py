e8e4157be0b64d6cd48e33926f25395fc43a6c41
Danil <fofogogo12345689@mail.ru>
2024-12-25 19:10:13 +0500

    adding functions to a tests views

---
+++ tests/test_views.py
@@ -0,0 +1,11 @@
+from src.views import main_page_view
+import json
+
+def test_main_page_view():
+    response = main_page_view("2024-06-25 12:00:00")
+    data = json.loads(response)
+    assert "greeting" in data
+    assert "cards" in data
+    assert "top_transactions" in data
+    assert "currency_rates" in data
+    assert "stock_prices" in data
